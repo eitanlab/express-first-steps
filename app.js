25b675e48ae70bb68eaf2acfdd8b17b7f58731b7
Eitan Lab <46536280+eitanlab@users.noreply.github.com>
2021-07-25 19:48:04 -0300

    Agrego body parser y armo lógica para enviar y recibir en 2 rutas diferentes

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/add-product', (req, res, next) => {
-	res.send('<h1>Add product</h1>');
+	res.send(
+		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</form>');
+});
+
+app.post('/product', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
