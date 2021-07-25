9af8b20f2f150783b7878ba63c516e3592dc630d
Eitan Lab <46536280+eitanlab@users.noreply.github.com>
2021-07-25 20:36:17 -0300

    modularizo las rutas

--- app.js
+++ app.js
@@ -2,20 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-	res.send(
-		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</form>');
-});
-
-app.post('/product', (req, res, next) => {
-	console.log(req.body);
-	res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-	res.send('<h1>Hello World</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
