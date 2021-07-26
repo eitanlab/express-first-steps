f2edc8964ee6bea6c8bc74d3f77352274e0b7149
Eitan Lab <46536280+eitanlab@users.noreply.github.com>
2021-07-25 23:50:18 -0300

    Cambio html generado inline por templates html

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
